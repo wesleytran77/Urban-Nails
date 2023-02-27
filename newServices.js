4d474bb7422473724051f51cb6760ffb72bede8d
Wesley Tran <122310175+wesleytran77@users.noreply.github.com>
2023-02-27 01:38:58 -0600

    Add files via upload

---
+++ newServices.js
@@ -0,0 +1,16 @@
+const callButton= document.querySelector(".contact")
+
+//handles call button
+callButton.addEventListener("click", function(){
+    window.location.href = "tel:+9568544541"
+})
+
+
+//home button functionality
+const home= document.querySelector(".title")
+
+home.addEventListener("click", function(){
+    document.location.href= "index.html"
+})
+
+//change all numbers to theirs
